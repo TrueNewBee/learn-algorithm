af162859e83a1e83ad3a7abff4eaed0160088b02
TrueNewBee <1076176004@qq.com>
2023-11-06 22:14:58 +0800

    init

---
+++ src/main/java/vip/chentianxiang/LearnAlgorithmApplication.java
@@ -0,0 +1,13 @@
+package vip.chentianxiang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnAlgorithmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnAlgorithmApplication.class, args);
+    }
+
+}
